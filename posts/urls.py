1a74491733c64f1356ad5ab7c880a9edd89848c3
Arian27Kapllanaj <ariankapllanaj@outlook.com>
2022-01-18 20:27:11 +0100

    showing data from db

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from .views import home_view
+from .views import home_view, post_view_json
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', home_view, name='home-view'),
+
+    # endpoints
+    path('posts-json/', post_view_json, name='posts-view-json'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,19 @@
+from django.http import request
 from django.shortcuts import render
-
+from .models import Post
+from django.http import JsonResponse
+from django.core import serializers
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'posts/main.html', {'hello': 'hello world'})
+    qs = Post.objects.all()
+    context = {
+        'hello': 'hello world',
+        'qs':qs
+    }
+    return render(request, 'posts/main.html', context)
+
+def post_view_json(request):
+    qs = Post.objects.all()
+    data = serializers.serialize('json', qs)
+    return JsonResponse({'data':data})

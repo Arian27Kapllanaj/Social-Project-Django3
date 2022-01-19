7435575c0b4d152534fdfad2ad73a4c19bb61c28
Arian27Kapllanaj <ariankapllanaj@outlook.com>
2022-01-19 19:52:09 +0100

    adding profile model

--- social/urls.py
+++ social/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
 ]
 
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
